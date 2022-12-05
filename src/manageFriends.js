e869ae88150168c9df56eef79256e27d22eb08e3
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-12-05 03:26:44 +0300

    completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
-export function manageFriends(state, action) {
+export function manageFriends(state = { friends: [] }, action) {
   // your code here
+
+  switch (action.type) {
+    case "friends/add":
+      return { "friends": [...state.friends, action.payload] }
+    case "friends/remove":
+      let removed = state.friends.filter((friends) => friends.id !== action.payload)
+      return { "friends": removed }
+    default:
+      return state
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,16 @@
 const initialState = { numberOfPresents: 0 };
 
+// action = {
+//   type: "presents/increase",
+// };
+
 export function managePresents(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return{numberOfPresents: state.numberOfPresents + 1}
+  
+    default:
+      return state
+  }
 }
