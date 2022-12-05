1b83fe2c9cc944360807193c89ce06617e104c76
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-12-05 03:27:40 +0300

    completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -3,7 +3,7 @@ export function manageFriends(state = { friends: [] }, action) {
 
   switch (action.type) {
     case "friends/add":
-      return { "friends": [...state.friends, action.payload] }
+      return { friends: [...state.friends, action.payload] }
     case "friends/remove":
       let removed = state.friends.filter((friends) => friends.id !== action.payload)
       return { "friends": removed }
